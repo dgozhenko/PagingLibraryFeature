75b6eba1c8fc9e6096ba4c1ac591dcd9b7d9bef6
Daniil Hozhenko <daniil.gozhenko@gmail.com>
2020-10-02 21:33:41 +0300

    Create view model

---
+++ app/src/main/java/com/example/paginglibraryfeature/post_screen/PostViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.paginglibraryfeature.post_screen
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.paging.DataSource
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
+import com.example.paginglibraryfeature.api_model.RedditPost
+import com.example.paginglibraryfeature.data_source.PostsDataSource
+import kotlinx.coroutines.Dispatchers
+
+class PostViewModel: ViewModel() {
+    var postLiveData: LiveData<PagedList<RedditPost>>
+
+    init {
+        val config = PagedList.Config.Builder()
+            .setPageSize(30)
+            .setEnablePlaceholders(false)
+            .build()
+        postLiveData = initializedPagedListBuilder(config).build()
+    }
+
+    fun getPosts(): LiveData<PagedList<RedditPost>> = postLiveData
+
+    private fun initializedPagedListBuilder(config: PagedList.Config):
+            LivePagedListBuilder<String, RedditPost> {
+        val dataSourceFactory = object: DataSource.Factory<String, RedditPost>() {
+            override fun create(): DataSource<String, RedditPost> {
+                return PostsDataSource(Dispatchers.IO)
+            }
+        }
+        return LivePagedListBuilder(dataSourceFactory, config)
+    }
+}
