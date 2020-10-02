4737247db04646468e8112861a4059a761f8c3e6
Daniil Hozhenko <daniil.gozhenko@gmail.com>
2020-10-02 22:13:47 +0300

    Initialize list to recycler view

--- app/src/main/java/com/example/paginglibraryfeature/MainActivity.kt
+++ app/src/main/java/com/example/paginglibraryfeature/MainActivity.kt
@@ -2,10 +2,52 @@ package com.example.paginglibraryfeature
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.paginglibraryfeature.post_screen.PostViewModel
+import com.example.paginglibraryfeature.post_screen.RedditPostAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_recycler_view.*
 
 class MainActivity : AppCompatActivity() {
+    private val redditPostAdapter = RedditPostAdapter()
+    lateinit var postViewModel: PostViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        postViewModel = ViewModelProvider(this).get(PostViewModel::class.java)
+
+        observeLiveData()
+        initializeList()
     }
-}
+
+    private fun observeLiveData() {
+        postViewModel.postLiveData.observe(this, Observer {
+            redditPostAdapter.submitList(it)
+        })
+    }
+
+    private fun initializeList() {
+        list.layoutManager = LinearLayoutManager(this)
+        list.adapter = redditPostAdapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+ }
